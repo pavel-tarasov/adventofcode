4da4db92a0bbc0b5dc374594b72b364f5e0f9b91
pavel-tarasov <pavel.a.tarasov@gmail.com>
2022-12-05 00:21:49 +0200

    Added boilerplate code

---
+++ python/day_5.py
@@ -0,0 +1,12 @@
+task_1_result = 0
+task_2_result = 0
+
+with open("../inputs/day_5.txt") as file:
+    while True:
+        line = file.readline().replace("\n", "")
+        if line == "":
+            print("file processed")
+            break
+
+print(f"task 1 result: {task_1_result}")
+print(f"task 2 result: {task_2_result}")
