86a5071e9c0244e5f640c7f245153f32ed4e12f6
Pavel Tarasov <pavel.t@nano-di.com>
2022-12-08 00:04:32 +0200

    Added boilerplate code

---
+++ python/day_8.py
@@ -0,0 +1,9 @@
+task_1_result = 0
+task_2_result = 0
+
+with open("../inputs/day_8.txt") as file:
+    while True:
+        line = file.readline().replace("\n", "")
+        if line == "":
+            print("file processed")
+            break
