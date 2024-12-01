ba1db38d23db24903f4a65a421ed6cf8cb0f7a27
Pavel Tarasov <pavel.tarasov@nano-di.com>
2024-12-01 10:54:04 +0200

    Day 1 ready

--- 2024/day_01/task_1.py
+++ 2024/day_01/task_1.py
@@ -1,4 +1,7 @@
 result = None
+list_a = []
+list_b = []
+diffs = []
 with open("input.txt") as file:
     while True:
         line = file.readline()
@@ -7,4 +10,20 @@ with open("input.txt") as file:
             print("file processed")
             break
 
+        # print(line)
+        a, b = line.split("   ")
+        list_a.append(int(a))
+        list_b.append(int(b))
+
+# print(list_a[0])
+# print(list_b[0])
+assert len(list_a) == len(list_b)
+list_a.sort()
+list_b.sort()
+for i, a in enumerate(list_a):
+    b = list_b[i]
+    # print(f"abs({a} - {b}) = {abs(a - b)}")
+    diffs.append(abs(a - b))
+
+result = sum(diffs)
 print(f"task 1 result: {result}")
--- 2024/day_01/task_2.py
+++ 2024/day_01/task_2.py
@@ -1,4 +1,7 @@
 result = None
+list_a = []
+list_b = []
+occurrences = []
 with open("input.txt") as file:
     while True:
         line = file.readline()
@@ -7,4 +10,20 @@ with open("input.txt") as file:
             print("file processed")
             break
 
-print(f"task 1 result: {result}")
+        # print(line)
+        a, b = line.split("   ")
+        list_a.append(int(a))
+        list_b.append(int(b))
+
+# print(list_a[0])
+# print(list_b[0])
+assert len(list_a) == len(list_b)
+# list_a.sort()
+# list_b.sort()
+for a in list_a:
+    count = list_b.count(a)
+    print(f"{a} occurs {count} times")
+    occurrences.append(a * count)
+
+result = sum(occurrences)
+print(f"task 2 result: {result}")
