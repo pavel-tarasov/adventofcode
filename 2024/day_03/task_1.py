e31cc65e9420e07cf7b556bcad1b9a113011a6c8
Pavel Tarasov <pavel.tarasov@nano-di.com>
2024-12-03 12:59:41 +0200

    Day 3 done

--- 2024/day_03/task_1.py
+++ 2024/day_03/task_1.py
@@ -1,10 +1,25 @@
+import re
+
+
 result = None
+memory_string = ""
 with open("input.txt") as file:
     while True:
         line = file.readline()
+        memory_string += line
 
         if line == "":
             print("file processed")
             break
 
-print(f"task 1 result: {result}")
+print(memory_string)
+
+commands = re.findall("mul\(\d+,\d+\)", memory_string)
+print(commands)
+
+sum = 0
+for command in commands:
+    a, b = list(map(int, command.strip("mul()").split(",")))
+    sum += a * b
+
+print(f"task 1 result: {sum}")
--- 2024/day_03/task_2.py
+++ 2024/day_03/task_2.py
@@ -1,10 +1,47 @@
+import re
+from asyncio.constants import FLOW_CONTROL_HIGH_WATER_SSL_READ
+
 result = None
+memory_string = ""
 with open("input.txt") as file:
     while True:
         line = file.readline()
+        memory_string += line
 
         if line == "":
             print("file processed")
             break
 
-print(f"task 1 result: {result}")
+# print(memory_string)
+
+commands = []
+for match in re.finditer("mul\(\d+,\d+\)", memory_string):
+    commands.append({"position": match.start(), "expression": match.group(0)})
+# print(commands)
+
+control_commands = [{"position": 0, "enabled": True}]
+for match in re.finditer("do\(\)", memory_string):
+    control_commands.append({"position": match.start(), "enabled": True})
+    for match in re.finditer("don't\(\)", memory_string):
+        control_commands.append({"position": match.start(), "enabled": False})
+control_commands.sort(key=lambda e: e["position"])
+# print(control_commands)
+
+sum = 0
+current_control_position = 0
+for command in commands:
+    enabled = None
+    for i in range(current_control_position, len(control_commands)):
+        if (control_commands[i]["position"] <= command["position"]) and (
+            i == len(control_commands) - 1
+            or command["position"] <= control_commands[i + 1]["position"]
+        ):
+            enabled = control_commands[i]["enabled"]
+            current_control_position = i
+            break
+    assert enabled is not None
+    if enabled:
+        a, b = list(map(int, command["expression"].strip("mul()").split(",")))
+        sum += a * b
+
+print(f"task 2 result: {sum}")
